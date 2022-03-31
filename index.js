c731937702bf84a94260d2ae26fcf5c181dd3921
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-03-31 11:25:58 -0700

    still need to resolve the sequelize issue

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict';
 
+require('dotenv').config();
+
 // Start up DB Server
 const { db } = require('./src/auth/models/index.js');
+const { server } = require('./src/server');
+
+const PORT = process.env.PORT || 3001;
+
 db.sync()
   .then(() => {
 
     // Start the web server
-    require('./src/server.js').start(process.env.PORT);
+    server.startup(PORT);
   });
